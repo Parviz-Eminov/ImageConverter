7e2b3b6520c48f7186762406d066377657a98fbb
Parviz-Eminov <parvizeminov75@gmail.com>
2025-03-29 14:13:26 +0400

    Create TextColorSchemaImplements.java

---
+++ TextColorSchemaImplements.java
@@ -0,0 +1,70 @@
+package ru.netology.graphics.image;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+import java.net.URL;
+
+public class TextGraphicsConverterImplements implements TextGraphicsConverter {
+    private int maxWidth;
+    private int maxHeight;
+    private double maxRatio;
+    private TextColorSchema schema;
+
+    @Override
+    public String convert(String url) throws IOException, BadImageSizeException {
+        BufferedImage img = ImageIO.read(new URL(url));
+        int width = img.getWidth();
+        int height = img.getHeight();
+        double ratio = (double) width / height;
+        if (ratio > maxRatio) {
+            throw new BadImageSizeException(ratio, maxRatio);
+        }
+        double scale = Math.min((double) maxWidth / width, (double) maxHeight / height);
+        int newWidth = (int) (width * scale);
+        int newHeight = (int) (height * scale);
+
+
+        Image scaledImage = img.getScaledInstance(newWidth, newHeight, BufferedImage.SCALE_SMOOTH);
+        BufferedImage bwImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_BYTE_GRAY);
+        Graphics2D graphics = bwImg.createGraphics();
+        graphics.drawImage(scaledImage, 0, 0, null);
+
+
+        WritableRaster bwRaster = bwImg.getRaster();
+        StringBuilder symvols = new StringBuilder();
+
+        for (int h = 0; h < newHeight; h++) {
+            for (int w = 0; w < newWidth; w++) {
+                int color = bwRaster.getPixel(w, h, new int[3])[0];
+                char c = schema.convert(color);
+                symvols.append(c).append(c);
+            }
+            symvols.append("\n");
+        }
+
+        return symvols.toString();
+    }
+
+    @Override
+    public void setMaxWidth(int width) {
+        this.maxWidth = width;
+    }
+
+    @Override
+    public void setMaxHeight(int height) {
+        this.maxHeight = height;
+    }
+
+    @Override
+    public void setMaxRatio(double maxRatio) {
+        this.maxRatio = maxRatio;
+    }
+
+    @Override
+    public void setTextColorSchema(TextColorSchema schema) {
+        this.schema = schema;
+    }
+}
